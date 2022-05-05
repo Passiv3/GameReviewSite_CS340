b81bb2f183d1face75d8fd732d166c68b0f52de5
Brian Dinh <dinhbr@flip2.engr.oregonstate.edu>
2022-05-04 19:18:29 -0700

    changed app.py to add data and changes to games.html to display

--- app.py
+++ app.py
@@ -6,44 +6,7 @@ import os
 app = Flask(__name__)
 
 # Routes 
-
-
-
-@app.route('/')
-def root():
-    return render_template("index.html")
-
-@app.route('/games')
-def gamePage():
-    return render_template("games.html")
-
-@app.route('/developers')
-def developerPage():
-    return render_template("developers.html")
-
-@app.route('/genres')
-def genrePage():
-    return render_template("genres.html")
-
-@app.route('/reviewers')
-def reviewerPage():
-    return render_template("reviewers.html")
-
-@app.route('/reviews')
-def reviewsPage():
-    return render_template("reviews.html")
-
-# Listener
-
-
-if __name__ == "__main__":
-    # Port is second argument here
-    port = int(os.environ.get('PORT', 59123)) 
-    
-    app.run(port=port, debug = True) 
-
-Games =
-[
+Games =[
 {
     "game_id": "1",
     "Developers_developer_id": 1,
@@ -52,44 +15,73 @@ Games =
 }
 ]
 
-Developers = 
-[
+Developers = [
 {
-    "developer_id": "1"
+    "developer_id": "1",
     "developer_name": "Ghost Games"
 }
 ]
 
-Reviews = 
-[
+Reviews = [
 {
-    "review_id": "1"
-    "Games_games_id": "1"
-    "Reviewers_reviewer_id": "1"
+    "review_id": "1",
+    "Games_games_id": "1",
+    "Reviewers_reviewer_id": "1",
     "review_date": "rating"
 }
 ]
 
-Reviewers =
-[
+Reviewers =[
 {
-    "reviewer_id": "1"
-    "reviewer_name": "Gamecritic"
+    "reviewer_id": "1",
+    "reviewer_name": "Gamecritic",
     "number_of_review": 2
 }
 ]
 
-GameGenres = 
-[
+GameGenres = [
 {
-    "Games_game_id" = "1"
-    "Genres_genre_id" = "1"
+    "Games_game_id" : "1",
+    "Genres_genre_id" : "1"
 }
 ]    
-Genres = 
-[
+Genres = [
 {
-    "genre_id": "1"
+    "genre_id": "1",
     "game_genre": "Racing"
 }
 ]
+
+
+@app.route('/')
+def root():
+    return render_template("index.html")
+
+@app.route('/games')
+def gamePage():
+    return render_template("games.html", games = Games)
+
+@app.route('/developers')
+def developerPage():
+    return render_template("developers.html", developers = Developers)
+
+@app.route('/genres')
+def genrePage():
+    return render_template("genres.html", genres = Genres)
+
+@app.route('/reviewers')
+def reviewerPage():
+    return render_template("reviewers.html", reviewers = Reviewers)
+
+@app.route('/reviews')
+def reviewsPage():
+    return render_template("reviews.html", reviews = Reviews)
+
+# Listener
+
+
+if __name__ == "__main__":
+    # Port is second argument here
+    port = int(os.environ.get('PORT', 59124)) 
+    
+    app.run(port=port, debug = True) 
