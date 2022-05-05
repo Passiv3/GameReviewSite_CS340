6c4000482f27a5f113111a0d452f309aadc93d51
Danny Chung <chundann@flip2.engr.oregonstate.edu>
2022-05-05 11:40:55 -0700

    Refactored some more code for general readability, fixed some typos, removed some lines, added more citations, began removing hardcoded selects, wip

--- app.py
+++ app.py
@@ -7,19 +7,56 @@ app = Flask(__name__, template_folder='templates')
 
 # Routes 
 
+# Temporary hardcoded data for step 2, these will be replaced with calls to a db in the next step
 Games = [
 {
     "game_id": "1",
     "Developers_developer_id": 1,
     "game_name": "Need for Speed Heat",
     "release_date": "2019-11-08"
-}
+},
+{
+    "game_id": "2",
+    "Developers_developer_id": 2,
+    "game_name": "Counter Strike: Global Offensive",
+    "release_date": "2012-08-21"
+},
+{
+    "game_id": "3",
+    "Developers_developer_id": 3,
+    "game_name": "Monster Hunter: World",
+    "release_date": "2018-08-09"
+},
+{
+    "game_id": "4",
+    "Developers_developer_id": 3,
+    "game_name": "Devil May Cry V",
+    "release_date": "2019-03-08"
+},
+{
+    "game_id": "5",
+    "Developers_developer_id": 4,
+    "game_name": "Death Stranding",
+    "release_date": "2019-11-08"
+},
 ]
 
 Developers = [
 {
     "developer_id": "1",
     "developer_name": "Ghost Games"
+},
+{
+    "developer_id": "2",
+    "developer_name": "Valve"
+},
+{
+    "developer_id": "3",
+    "developer_name": "CAPCOM"
+},
+{
+    "developer_id": "4",
+    "developer_name": "Kojia Production"
 }
 ]
 
@@ -47,12 +84,33 @@ GameGenres = [
     "Games_game_id" : "1",
     "Genres_genre_id" : "1"
 }
-]    
+]
+
 Genres = [
 {
     "genre_id": "1",
     "game_genre": "Racing"
-}
+},
+{
+    "genre_id": "2",
+    "game_genre": "FPS"
+},
+{
+    "genre_id": "3",
+    "game_genre": "Shooter"
+},
+{
+    "genre_id": "4",
+    "game_genre": "Adventure"
+},
+{
+    "genre_id": "5",
+    "game_genre": "Hack-And-Slash"
+},
+{
+    "genre_id": "6",
+    "game_genre": "Action"
+},
 ]
 
 GamesGenre = [
@@ -62,17 +120,13 @@ GamesGenre = [
 }
 ]
 
-@app.route('/gameGenres')
-def gameGenres():
-    return render_template("gameGenres.html", gameGenres = GamesGenre)
-
 @app.route('/')
 def root():
     return render_template("index.html")
 
 @app.route('/games')
 def gamePage():
-    return render_template("games.html", games = Games)
+    return render_template("games.html", games = Games, developers = Developers)
 
 @app.route('/developers')
 def developerPage():
@@ -89,6 +143,10 @@ def reviewerPage():
 @app.route('/reviews')
 def reviewsPage():
     return render_template("reviews.html", reviews = Reviews)
+
+@app.route('/gameGenres')
+def gameGenres():
+    return render_template("gameGenres.html", gameGenres = GamesGenre, games = Games, genres = Genres)
 
 # Listener
 
