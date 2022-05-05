b7e3e5541eaca5751234c03e85e9b498a8b025af
Danny Chung <chundann@flip2.engr.oregonstate.edu>
2022-05-04 17:12:38 -0700

    added html files

--- app.py
+++ app.py
@@ -11,35 +11,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def root():
-    return render_template("games.j2", people = people_from_app_py)
+    return render_template("index.html")
 
 # Listener
 
-people_from_app_py =[
-{
-    "name": "Thomas",
-    "age": 33,
-    "location": "New Mexico",
-    "favorite_color": "Blue"
-},
-{
-    "name": "Gregory",
-    "age": 41,
-    "location": "Texas",
-    "favorite_color": "Red"
-},
-{
-    "name": "Vincent",
-    "age": 27,
-    "location": "Ohio",
-    "favorite_color": "Green"
-},
-{
-    "name": "Alexander",
-    "age": 29,
-    "location": "Florida",
-    "favorite_color": "Orange"
-}]
 
 if __name__ == "__main__":
     # Port is second argument here
---
+++ database/db_credentials.py
@@ -0,0 +1,4 @@
+host = 'classmysql.engr.oregonstate.edu'
+user = 'cs340_chundann'
+passwd = '8473'
+db = 'cs340_chundann'
